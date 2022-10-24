b60b0ba1481c898ea0639097d7e7884fe4afe595
Damian2057 <ddamian2057@gmail.com>
2022-10-24 20:24:49 +0200

    Init

---
+++ src/test/java/com/mobile/server/ServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mobile.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
